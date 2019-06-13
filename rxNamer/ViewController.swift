c795ffe3d1c121a3468e02cbf559f3688cf405f7
AHMED <ahmed@AHMEDs-MacBook-Pro.local>
2019-06-13 20:07:57 +0200

    Initial Commit

---
+++ rxNamer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxNamer
+//
+//  Created by AHMED on 3/23/1398 AP.
+//  Copyright © 1398 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
